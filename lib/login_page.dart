8bef833af4bbc148cd77f2f218c0010d8cd26087
brokoliboro <brokoli@marketboro.com>
2023-09-04 14:25:02 -0600

    added alignment

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -6,50 +6,32 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          print('button Clicked!');
-        },
-      ),
-      body: Column(
-        children: [
-          Text(
-            'Let\'s sign you in!!',
-            style: TextStyle(
-              fontSize: 30,
-              color: Colors.brown,
-              fontWeight: FontWeight.bold,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Let\'s sign you in!!',
+              style: TextStyle(
+                fontSize: 30,
+                color: Colors.brown,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          ),
-          Text(
-            'Welcome back! \nYou\'ve been missed!',
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.blueGrey,
-              fontWeight: FontWeight.bold,
+            Text(
+              'Welcome back! \nYou\'ve been missed!',
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.blueGrey,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          ),
-          Image.network(
-            'https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg',
-            height: 200,
-          ),
-          Container(
-            height: 150,
-            width: 150,
-            padding: EdgeInsets.all(10),
-            margin: EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                  fit: BoxFit.fitWidth,
-                  image: NetworkImage(
-                    'https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg',
-                  )),
-              color: Colors.blue,
-              borderRadius: BorderRadius.circular(24),
+            Image.network(
+              'https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg',
+              height: 200,
             ),
-          )
-        ],
+          ],
+        ),
       ),
     );
   }
