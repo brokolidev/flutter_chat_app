eccc3e76eb5976e11f7209548d724318af47b022
brokoliboro <brokoli@marketboro.com>
2023-09-11 19:07:11 -0600

    adding state variable and widget variables

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_chat_app/chat_page.dart';
+import 'package:flutter_chat_app/widgets/counter_stateful_demo.dart';
 
 import 'login_page.dart';
 import 'package:flutter/material.dart';
@@ -12,7 +13,7 @@ class ChatApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(primarySwatch: Colors.yellow),
-      home: ChatPage(),
+      home: CounterStateful(buttonColor: Colors.blue),
     );
   }
 }
---
+++ lib/widgets/counter_stateful_demo.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class CounterStateful extends StatefulWidget {
+  Color buttonColor;
+
+  CounterStateful({super.key, required this.buttonColor});
+
+  @override
+  State<CounterStateful> createState() {
+    State<CounterStateful> stateClassAssociateWithThisWidget =
+        _CounterStatefulState();
+    return stateClassAssociateWithThisWidget;
+  }
+}
+
+class _CounterStatefulState extends State<CounterStateful> {
+  int counter = 0;
+
+  void increment() {
+    if (mounted) {
+      setState(() {
+        counter++;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Counter"),
+      ),
+      body: Center(
+        child: Text(
+          '$counter',
+          style: const TextStyle(
+            fontSize: 30,
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: widget.buttonColor,
+        child: const Icon(Icons.add),
+        onPressed: () {
+          increment();
+        },
+      ),
+    );
+  }
+}
